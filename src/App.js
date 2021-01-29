35d926e77ddc599b9fe907049cbfaf16b20ff062
Lokesh8055 <lokeshdude7@hotmail.com>
2021-01-29 10:55:54 +0530

    Birthday Remainder Application created using React

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import data from './data';
+import List from './List';
+function App() {
+  const [people, setPeople] = useState(data);
+
+  const removePerson = (id) => {
+    let newPerson = people.filter(people => people.id !== id);
+    setPeople(newPerson);
+  }
+
+  return (
+    <main>
+      <section className="container">
+        <h3 className="center"><span className="birthday">{people.length}</span> birthday today</h3>
+        {people.map(person => (
+          <List key={person.id} {...person} removePerson={removePerson} />
+        ))}
+        <button type="button" className="btn-secondary" onClick={() => setPeople([])}>Clear All</button>
+      </section>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/List.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const List = (props) => {
+  const { id, name, age, image, removePerson } = props;
+
+  return (
+    <>
+      <article className="person">
+        <img src={image} alt={name} />
+        <div>
+          <h4>{name}</h4>
+          <p>{age} years</p>
+        </div>
+        <button className="btn-primary" onClick={() => removePerson(id)}>Remove</button>
+      </article>
+    </>
+  );
+};
+
+export default List;
